1aad0bdf3d2c8217a24144f1801085a5fa0672c5
Azael Vargas <azael@cosource.com.au>
2025-05-26 22:08:34 -0500

    Use image from gallery

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,3 +1,22 @@
+data "azurerm_shared_image_gallery" "test" {
+  name                = "TestVMsGallery"
+  resource_group_name = "${var.resource_group}"
+  location            = "${var.location}"
+}
+
+data "azurerm_shared_image" "test" {
+  name                = "LinuxTestVM"
+  gallery_name        = azurerm_shared_image_gallery.test.name
+  resource_group_name = "${var.resource_group}"
+  location            = "${var.location}"
+  os_type             = "Linux"
+  identifier {
+    publisher = "canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
+  }
+}
+
 resource "azurerm_network_interface" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
@@ -15,21 +34,16 @@ resource "azurerm_linux_virtual_machine" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  size                = "Standard_DS2_v2"
-  admin_username      = "azvargas"
+  size                = "Standard_B1ls_v2"
+  admin_username      = "udacityadmin"
   network_interface_ids = [azurerm_network_interface.test.id]
   admin_ssh_key {
-    username   = "azvargas"
+    username   = "udacityadmin"
     public_key = file("./id_rsa.pub")
   }
   os_disk {
-    caching           = "ReadWrite"
+    caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
+  source_image_id = data.azurerm_shared_image.test.id
 }
