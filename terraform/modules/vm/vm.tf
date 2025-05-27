57d33688c23ec8ce5ae6dd3987898f306d5474d0
Azael Vargas <azael@cosource.com.au>
2025-05-26 22:57:54 -0500

    Change VM size

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -26,7 +26,7 @@ resource "azurerm_linux_virtual_machine" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  size                = "Standard_B1ls_v2"
+  size                = "Standard_B1ls"
   admin_username      = "udacityadmin"
   network_interface_ids = [azurerm_network_interface.test.id]
   admin_ssh_key {
