d9a01c274270415e4feb293820fae838f39bcffb
Azael Vargas <azael@cosource.com.au>
2025-05-29 23:27:45 -0500

    Fix terraform web app definition

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -16,12 +16,11 @@ resource "azurerm_windows_web_app" "test" {
     "WEBSITE_RUN_FROM_PACKAGE" = 0
   }
 
-  application_stack = {
-    current_stack  = "dotnet"
-    dotnet_version = "v4.0"
-  }
-
   site_config {
     always_on = false
+    application_stack {
+      current_stack = "dotnet"
+      dotnet_version = "v4.0"
+    }
   }
 }
