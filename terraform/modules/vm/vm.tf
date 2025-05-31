cb611d9e0751a8c3f2e72b0cc663c21472f0d9d4
Azael Vargas <azael@cosource.com.au>
2025-05-31 18:29:36 -0500

    Make azureuser admin

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -27,11 +27,11 @@ resource "azurerm_linux_virtual_machine" "test" {
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
   size                = "Standard_B1ms"
-  admin_username      = "udacityadmin"
+  admin_username      = "azureuser"
   secure_boot_enabled = false
   network_interface_ids = [azurerm_network_interface.test.id]
   admin_ssh_key {
-    username   = "udacityadmin"
+    username   = "azureuser"
     public_key = file("./id_rsa.pub")
   }
   os_disk {
