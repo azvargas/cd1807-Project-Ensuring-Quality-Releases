b14a15fc0e936a4776540cdaee65dfdc1e5a275d
Azael Vargas <azael@cosource.com.au>
2025-06-01 21:02:02 -0500

    Format selenium messages as log

--- automatedtesting/selenium/add_remove_from_cart.py
+++ automatedtesting/selenium/add_remove_from_cart.py
@@ -4,10 +4,11 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
 import time
 import logging
+import datetime
 
 # Start the browser and login with standard_user
 def login (user, password):
-    print ('Starting the browser...')
+    print (f'{datetime.datetime.now()} - SeleniumTest: Starting the browser...')
     # --uncomment when running in Azure DevOps.
     options = ChromeOptions()
     options.add_argument("--headless") 
@@ -19,9 +20,9 @@ def login (user, password):
         "profile.password_manager_enabled": False}
     options.add_experimental_option("prefs", prefs)
     driver = webdriver.Chrome(options=options)
-    print ('Browser started successfully. Navigating to the demo page to login.')
+    print (f'{datetime.datetime.now()} - SeleniumTest: Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
-    print (f'Attempting to login into the store with the user {user}.')
+    print (f'{datetime.datetime.now()} - SeleniumTest: Attempting to login into the store with the user {user}.')
     driver.find_element(By.ID, "user-name").send_keys(user)
     driver.find_element(By.ID, "password").send_keys(password)
     driver.find_element(By.ID, "login-button").click()
@@ -30,40 +31,40 @@ def login (user, password):
     # Search for the shopping cart container to check if the login was successful
     shopping_cart = driver.find_elements(By.CSS_SELECTOR, ".shopping_cart_container")
     if len(shopping_cart) == 1:
-        print ('Login successful.')
+        print (f'{datetime.datetime.now()} - SeleniumTest: Login successful.')
     else:
-        print ('Login failed.')
+        print (f'{datetime.datetime.now()} - SeleniumTest: Login failed.')
     
     assert len(shopping_cart) == 1, "Login process failed."
     return driver
 
 def add_items_to_cart (driver):
-    print ('Adding products to cart.')
+    print (f'{datetime.datetime.now()} - SeleniumTest: Adding products to cart.')
     # Search for the products
     articles = driver.find_elements(By.CSS_SELECTOR, "div.inventory_item")
     total_articles = len(articles)
-    print (f'Found {total_articles} articles')
+    print (f'{datetime.datetime.now()} - SeleniumTest: Found {total_articles} articles')
     for article in articles:
         name = article.find_element(By.CSS_SELECTOR, ".inventory_item_name").text
         button = article.find_element(By.CSS_SELECTOR, "button.btn_inventory")
-        print (f'Adding {name} to the cart')
+        print (f'{datetime.datetime.now()} - SeleniumTest: Adding {name} to the cart')
         button.click()
 
     cart_icon = driver.find_element(By.CSS_SELECTOR, "a.shopping_cart_link")
     cart_item_count = int(cart_icon.text)
-    print (f'Articles in cart: {cart_item_count}')
+    print (f'{datetime.datetime.now()} - SeleniumTest: Articles in cart: {cart_item_count}')
     assert total_articles == cart_item_count, 'Not all the articles were added to the shopping cart'
 
 def remove_items_from_cart(driver):
-    print ('Removing products to cart.')
+    print (f'{datetime.datetime.now()} - SeleniumTest: Removing products to cart.')
     # Search for the products
     articles = driver.find_elements(By.CSS_SELECTOR, "div.inventory_item")
     total_articles = len(articles)
-    print (f'Found {total_articles} articles')
+    print (f'{datetime.datetime.now()} - SeleniumTest: Found {total_articles} articles')
     for article in articles:
         name = article.find_element(By.CSS_SELECTOR, ".inventory_item_name").text
         button = article.find_element(By.CSS_SELECTOR, "button.btn_inventory")
-        print (f'Removing {name} to the cart')
+        print (f'{datetime.datetime.now()} - SeleniumTest: Removing {name} to the cart')
         button.click()
 
     cart_icon = driver.find_element(By.CSS_SELECTOR, "a.shopping_cart_link")
@@ -72,9 +73,11 @@ def remove_items_from_cart(driver):
     else:
         cart_item_count = int(cart_icon.text)
 
-    print (f'Articles in cart: {cart_item_count}')
+    print (f'{datetime.datetime.now()} - SeleniumTest: Articles in cart: {cart_item_count}')
     assert cart_item_count == 0, 'Not all the articles were removed from the shopping cart'
 
+print (f'{datetime.datetime.now()} - SeleniumTest: Start testing.')
 driver = login('standard_user', 'secret_sauce')
 add_items_to_cart(driver)
 remove_items_from_cart(driver)
+print (f'{datetime.datetime.now()} - SeleniumTest: End testing.')
